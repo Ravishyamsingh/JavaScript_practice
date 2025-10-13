c18d7cb761cf3defe2336ea91ed52cb15d529203
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-10-13 21:19:24 +0530

    Add files via upload

---
+++ 03_typeConversion.js
@@ -0,0 +1,38 @@
+let score =33;
+let num=null
+let age=undefined
+
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let ValueInNumber=Number(score);
+console.log(typeof ValueInNumber);
+console.log(ValueInNumber);
+console.log(typeof num);
+console.log(num);
+
+console.log(typeof age);
+console.log(age);
+
+
+let string="5";
+let num2=3;
+console.log(string+num2);
+
+console.log("4"+3);
+
+console.log(10 + 5);
+console.log(10 - 5);
+console.log(10 * 5);
+console.log(10 / 5);
+console.log(10 % 5);
+
+
+let str1="5";
+console.log(str1+3);
+
+console.log("2">5);
+
+
+//https://tc39.es/ecma262/2023/multipage/#sec-intro
