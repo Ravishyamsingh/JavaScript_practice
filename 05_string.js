37e84c9a08f60dd54574fca718a69ccfdacc5629
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-10-13 21:19:39 +0530

    Add files via upload

---
+++ 05_string.js
@@ -0,0 +1,46 @@
+const name="shyamsingh"
+const repoCount=50;
+//console.log(name+repoCount+"word");
+console.log(`Helo, my name is ${name} and my repo count is ${repoCount}!`);
+
+
+const gameName=new String("Ravishyam");
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('i'));
+// console.log(gameName.substring(0,4));
+
+// const len=gameName.length;
+// console.log(len);
+
+console.log("helo".length);
+
+console.log("        jdgehjgd  ".trim());
+
+console.log("        jdgehjgd  8".trimStart());
+console.log("        jdgehjgd".trimEnd());
+
+console.log(name.charAt(1));
+console.table(name[2]);
+
+console.log("To kaise h aap".includes('h'));
+
+console.log("To kaise h aap".indexOf('s'));
+
+console.log("To kaise h aap".lastIndexOf('p'));
+
+console.log("To kaise h aap".startsWith('g'));
+
+console.log("To kaise h aap".endsWith('aap'));
+
+console.log("To kaise h aap".search('kaise'));
+
+
+let college="kalasalingam"
+console.log(college.slice(0,4));
+console.log(college.substring(0,college.length));
+
