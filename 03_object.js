2852595ec3e1600420a908b9f7af516158839553
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-10-14 21:29:34 +0530

    Add files via upload

---
+++ 03_object.js
@@ -0,0 +1,52 @@
+// singleton
+// Object.create
+
+// object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Ravi",
+    "full name": "Ravi Shyam",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "ravi@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+ console.log(JsUser[mySym])
+
+JsUser.email = "ravi@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "ravi@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
+
+
+const mySyms=Symbol("myKeys")
+
+const user1={
+    name:"Ravi",
+    age:20,
+    email:"ravi@google.com",
+    [mySyms]:"value"
+
+
+}
+console.log(user1.name)
+console.log(typeof mySyms);
