006b315ad9a5ce0e5a84f3a54d165577485e5418
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-10-15 17:55:07 +0530

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,47 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+
+const day="tues"
+
+switch (day){
+    case "Sun":
+        console.log(`Today sunday`);
+        break;
+    case "mon":
+        console.log(`Today Monday`);
+        break;
+    case "tues":
+        console.log(`Today Tuesday`);
+        break;
+    default:
+        console.log(`Days didn't match`);
+
+}
