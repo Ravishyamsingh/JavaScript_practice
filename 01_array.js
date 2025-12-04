de315a0faaa029a0d79fe81b4a0562d7624dc92c
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-12-04 15:53:15 +0530

    Update 01_array.js

--- 01_array.js
+++ 01_array.js
@@ -1,3 +1,9 @@
+
+
+
+
+
+
 // array
 
 const myArr = [0, 1, 2, 3, 4, 5]
@@ -36,4 +42,5 @@ console.log("B ", myArr);
 
 const myn2 = myArr.splice(1, 3)
 console.log("C ", myArr);
+
 console.log(myn2);
