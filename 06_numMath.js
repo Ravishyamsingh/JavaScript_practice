7f7caba5f6ff8d56f14117f22846f485c8f8cd4d
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-12-25 22:55:17 +0530

    Implement random number generation within range
    Added a random number generator within a specified range.

--- 06_numMath.js
+++ 06_numMath.js
@@ -1,3 +1,8 @@
+
+
+
+
+
 const score = 400
 // console.log(score);
 
@@ -30,5 +35,6 @@ console.log(Math.floor(Math.random()*10) + 1);
 
 const min = 10
 const max = 20
+
 
 console.log(Math.floor(Math.random() * (max - min + 1)) + min)
