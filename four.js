86cd1a47fbb5648acf8b81b1fcde8aef958056cb
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2026-02-01 20:45:40 +0530

    Update four.js

--- four.js
+++ four.js
@@ -1,26 +1,30 @@
+// Object iteration using for...in
 const myObject = {
-    js: 'javascript',
-    cpp: 'C++',
+    js: "javascript",
+    cpp: "C++",
     rb: "ruby",
     swift: "swift by apple"
-}
+};
 
 for (const key in myObject) {
-    //console.log(`${key} shortcut is for ${myObject[key]}`);
+    console.log(`${key} shortcut is for ${myObject[key]}`);
 }
 
-const programming = ["js", "rb", "py", "java", "cpp"]
 
-for (const key in programming) {
-    //console.log(programming[key]);
+// Array iteration (works, but not recommended for arrays in real code)
+const programming = ["js", "rb", "py", "java", "cpp"];
+
+for (const index in programming) {
+    console.log(programming[index]);
 }
 
-// const map = new Map()
-// map.set('IN', "India")
-// map.set('USA', "United States of America")
-// map.set('Fr', "France")
-// map.set('IN', "India")
 
-// for (const key in map) {
-//     console.log(key);
-// }
+// Correct way to iterate over Map
+const map = new Map();
+map.set("IN", "India");
+map.set("USA", "United States of America");
+map.set("FR", "France");
+
+for (const [key, value] of map) {
+    console.log(key, value);
+}
