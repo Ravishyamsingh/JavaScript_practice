eeb8277f9eedbbce48e3f5a65d8197f4cf1ad51e
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2026-02-01 20:47:23 +0530

    Refactor array transformation logic in seven.js
    Refactor variable names and improve readability of array transformations.

--- seven.js
+++ seven.js
@@ -1,10 +1,9 @@
-const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// const newNums = myNumers.map( (num) => { return num + 10})
+// Transform and filter the array in a natural, readable way
+const result = numbers
+    .map(num => num * 10)
+    .map(num => num + 1)
+    .filter(num => num >= 40);
 
-const newNums = myNumers
-                .map((num) => num * 10 )
-                .map( (num) => num + 1)
-                .filter( (num) => num >= 40)
-
-console.log(newNums);
+console.log(result);
