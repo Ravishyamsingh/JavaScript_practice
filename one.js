ceda98d157f3d14288fca8cf47954b81abf968a6
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2026-02-01 20:46:48 +0530

    Update one.js

--- one.js
+++ one.js
@@ -1,54 +1,44 @@
-
-
-
-
-// for
-
+// Basic for loop
 for (let i = 0; i <= 10; i++) {
-    const element = i;
-    if (element == 5) {
-        //console.log("5 is best number");
+    if (i === 5) {
+        console.log("5 is a special number");
     }
-    //console.log(element);
-    
+    console.log(i);
 }
 
-// console.log(element);
 
+// Nested loops (multiplication tables)
 for (let i = 1; i <= 10; i++) {
-    //console.log(`Outer loop value: ${i}`);
-   for (let j = 1; j <= 10; j++) {
-    //console.log(`Inner loop value ${j} and inner loop ${i}`);
-    //console.log(i + '*' + j + ' = ' + i*j );
-   }
-    
-}
-let myArray = ["flash", "batman", "superman"]
-//console.log(myArray.length);
-for (let index = 0; index < myArray.length; index++) {
-    const element = myArray[index];
-    //console.log(element);
-    
-}
-
-
-// break and continue
-
-// for (let index = 1; index <= 20; index++) {
-//     if (index == 5) {
-//         console.log(`Detected 5`);
-//         break
-//     }
-//    console.log(`Value of i is ${index}`);
-    
-// }
-
-for (let index = 1; index <= 20; index++) {
-    if (index == 5) {
-        console.log(`Detected 5`);
-        continue
+    console.log(`Table of ${i}`);
+    for (let j = 1; j <= 10; j++) {
+        console.log(`${i} * ${j} = ${i * j}`);
     }
-   console.log(`Value of i is ${index}`);
-    
+}
 
+
+// Looping through an array
+const heroes = ["flash", "batman", "superman"];
+
+for (let index = 0; index < heroes.length; index++) {
+    console.log(heroes[index]);
+}
+
+
+// Using break
+for (let index = 1; index <= 20; index++) {
+    if (index === 5) {
+        console.log("Detected 5, stopping loop");
+        break;
+    }
+    console.log(`Value of i is ${index}`);
+}
+
+
+// Using continue
+for (let index = 1; index <= 20; index++) {
+    if (index === 5) {
+        console.log("Detected 5, skipping this iteration");
+        continue;
+    }
+    console.log(`Value of i is ${index}`);
 }
